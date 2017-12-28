dc1538f6a7b9974ed78196561e4a5f900b4877c3
Artur Brodzki <artur@brodzki.org>
2017-12-29 00:46:23 +0100

    initial commit

---
+++ CMakeFiles/MED.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MED.dir/main.cpp.o"
+  "MED.pdb"
+  "MED"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MED.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
