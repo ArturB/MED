8be4daa690f5f864f289aae6ec037aaf9b5ba2a8
Artur Brodzki <artur@brodzki.org>
2017-12-29 01:02:22 +0100

    some cmake macro

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include "MedConfig.h"
+ 
 int main (int argc, char *argv[])
 {
   if (argc < 2)
     {
+    fprintf(stdout,"%s Version %d.%d\n",
+            argv[0],
+            Tutorial_VERSION_MAJOR,
+            Tutorial_VERSION_MINOR);
     fprintf(stdout,"Usage: %s number\n",argv[0]);
     return 1;
     }
@@ -14,4 +20,4 @@ int main (int argc, char *argv[])
   fprintf(stdout,"The square root of %g is %g\n",
           inputValue, outputValue);
   return 0;
-} 
+}
